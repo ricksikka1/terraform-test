2ea9ae82453ba64d54989868075050a9a3025774
Rick.Sikka <rick.sikka@tulip.com>
2020-08-20 20:37:11 -0400

    updated output vars

--- main.tf
+++ main.tf
@@ -9,3 +9,12 @@ resource "aws_instance" "ec2_instance" {
   ami = "ami-0cf31d971a3ca20d6"
   instance_type = "t2.micro"
 }
+
+output "ip" {
+  value = "${aws_instance.ec2_instance.public_ip}"
+}
+
+resource "aws_instance" "ec2_instance" {
+ ami = "ami-0cf31d971a3ca20d6"
+ instance_type = var.instance_type
+}
